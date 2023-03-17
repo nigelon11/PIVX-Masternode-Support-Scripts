79488039514c32df7da4caa4fd71fe3d969dcda0
nigelon11 <109360225+nigelon11@users.noreply.github.com>
2023-03-17 11:13:08 -0400

    Updated to have a consistent launch path.   This will help the MN monitoring software.

--- startAllMasternodes.sh
+++ startAllMasternodes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd ~/pivx-5.4.0/bin
-./pivxd $1
-./pivxd -datadir=/root/.pivx2 $1
-./pivxd -datadir=/root/.pivx3 $1
-./pivxd -datadir=/root/.pivx4 $1
+/root/pivx-5.5.0/bin/pivxd -datadir=/root/.pivx $1
+/root/pivx-5.5.0/bin/pivxd -datadir=/root/.pivx2 $1
+/root/pivx-5.5.0/bin/pivxd -datadir=/root/.pivx3 $1
+/root/pivx-5.5.0/bin/pivxd -datadir=/root/.pivx4 $1
 exit
